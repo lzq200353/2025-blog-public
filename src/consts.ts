2a36825e35792846477c1173bb79fed2b4e199e0
meow law <148182544+lzq200353@users.noreply.github.com>
2026-03-27 16:28:01 +0800

    Update GITHUB_APP_ID constant in consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -11,6 +11,6 @@ export const GITHUB_CONFIG = {
 	OWNER: process.env.NEXT_PUBLIC_GITHUB_OWNER || 'lzq200353',
 	REPO: process.env.NEXT_PUBLIC_GITHUB_REPO || '2025-blog-public',
 	BRANCH: process.env.NEXT_PUBLIC_GITHUB_BRANCH || 'main',
-	APP_ID: process.env.NEXT_PUBLIC_GITHUB_APP_ID || '3197769',
+	APP_ID: process.env.NEXT_PUBLIC_GITHUB_APP_ID || '3199724',
 	ENCRYPT_KEY: process.env.NEXT_PUBLIC_GITHUB_ENCRYPT_KEY || 'wudishiduomejimo',
 } as const
